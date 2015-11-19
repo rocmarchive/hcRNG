b4b8e6e2293aa22faf1b71e6e7380ff7d5878510
aparnasuresh <aparna@multicorewareinc.com>
2015-11-19 17:06:25 +0530

    Add Shell script files for building in Linux

---
+++ Build/linux/build.sh
@@ -0,0 +1,7 @@
+CLANG_PATH="/opt/clamp/bin/clang++"
+
+if [ -x $CLANG_PATH ]; then
+       cmake ../../source/library -DCMAKE_C_COMPILER=/opt/clamp/bin/clang  -DCMAKE_CXX_COMPILER=/opt/clamp/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
+else
+       cmake ../../source/library -DCMAKE_C_COMPILER=${MCWCPPAMPBUILD}/compiler/bin/clang  -DCMAKE_CXX_COMPILER=${MCWCPPAMPBUILD}/compiler/bin/clang++ -DCMAKE_CXX_FLAGS=-fPIC
+fi
---
+++ Build/linux/clean.sh
@@ -0,0 +1,5 @@
+rm -rf CMake*
+rm Makefile
+rm cmake*
+rm libhcrng.so
+rm install_manifest.txt
