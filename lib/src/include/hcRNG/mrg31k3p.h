18db334a454c32460e71cd90ed67bc8ea8017c3d
aparnasuresh <aparna@multicorewareinc.com>
2015-12-17 11:49:32 +0530

    Adding MRG31K3P Random number generator

---
+++ lib/src/include/hcRNG/mrg31k3p.h
@@ -0,0 +1,195 @@
+/*  @file mrg31k3p.h
+ *  @brief Specific interface for the MRG31k3p generator
+ *  @see hcRNG_template.h
+ */
+
+#pragma once
+#ifndef MRG31K3P_H
+#define MRG31K3P_H
+#include "hcRNG.h"
+#include <stdio.h>
+
+/*  @brief State type of a MRG31k3p stream
+ *  The state is a seed consisting of six unsigned 32-bit integers.
+ *  @see hcrngStreamState
+ */
+typedef struct {
+    /*! @brief Seed for the first MRG component
+     */
+    unsigned int g1[3];
+    /*! @brief Seed for the second MRG component
+     */
+    unsigned int g2[3];
+} hcrngMrg31k3pStreamState;
+
+
+struct hcrngMrg31k3pStream_ {
+	union {
+		struct {
+			hcrngMrg31k3pStreamState states[3];
+		};
+		struct {
+			hcrngMrg31k3pStreamState current;
+			hcrngMrg31k3pStreamState initial;
+			hcrngMrg31k3pStreamState substream;
+		};
+	};
+};
+
+/*! @copybrief hcrngStream
+ *  @see hcrngStream
+ */
+typedef struct hcrngMrg31k3pStream_ hcrngMrg31k3pStream;
+
+struct hcrngMrg31k3pStreamCreator_;
+/*! @copybrief hcrngStreamCreator
+ *  @see hcrngStreamCreator
+ */
+typedef struct hcrngMrg31k3pStreamCreator_ hcrngMrg31k3pStreamCreator;
+
+/*! @copybrief hcrngCopyStreamCreator()
+ *  @see hcrngCopyStreamCreator()
+ */
+HCRNGAPI hcrngMrg31k3pStreamCreator* hcrngMrg31k3pCopyStreamCreator(const hcrngMrg31k3pStreamCreator* creator, hcrngStatus* err);
+
+/*! @copybrief hcrngDestroyStreamCreator()
+ *  @see hcrngDestroyStreamCreator()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pDestroyStreamCreator(hcrngMrg31k3pStreamCreator* creator);
+
+/*! @copybrief hcrngRewindStreamCreator()
+ *  @see hcrngRewindStreamCreator()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pRewindStreamCreator(hcrngMrg31k3pStreamCreator* creator);
+
+/*! @copybrief hcrngSetBaseCreatorState()
+ *  @see hcrngSetBaseCreatorState()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pSetBaseCreatorState(hcrngMrg31k3pStreamCreator* creator, const hcrngMrg31k3pStreamState* baseState);
+
+/*! @copybrief hcrngChangeStreamsSpacing()
+ *  @see hcrngChangeStreamsSpacing()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pChangeStreamsSpacing(hcrngMrg31k3pStreamCreator* creator, int e, int c);
+
+/*! @copybrief hcrngAllocStreams()
+ *  @see hcrngAllocStreams()
+ */
+HCRNGAPI hcrngMrg31k3pStream* hcrngMrg31k3pAllocStreams(size_t count, size_t* bufSize, hcrngStatus* err);
+
+/*! @copybrief hcrngDestroyStreams()
+ *  @see hcrngDestroyStreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pDestroyStreams(hcrngMrg31k3pStream* streams);
+
+/*! @copybrief hcrngCreateOverStreams()
+ *  @see hcrngCreateOverStreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pCreateOverStreams(hcrngMrg31k3pStreamCreator* creator, size_t count, hcrngMrg31k3pStream* streams);
+
+/*! @copybrief hcrngCreateStreams()
+ *  @see hcrngCreateStreams()
+ */
+HCRNGAPI hcrngMrg31k3pStream* hcrngMrg31k3pCreateStreams(hcrngMrg31k3pStreamCreator* creator, size_t count, size_t* bufSize, hcrngStatus* err);
+
+/*! @copybrief hcrngCopyOverStreams()
+ *  @see hcrngCopyOverStreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pCopyOverStreams(size_t count, hcrngMrg31k3pStream* destStreams, const hcrngMrg31k3pStream* srcStreams);
+
+/*! @copybrief hcrngCopyStreams()
+ *  @see hcrngCopyStreams()
+ */
+HCRNGAPI hcrngMrg31k3pStream* hcrngMrg31k3pCopyStreams(size_t count, const hcrngMrg31k3pStream* streams, hcrngStatus* err);
+
+#define hcrngMrg31k3pRandomU01          _HCRNG_TAG_FPTYPE(hcrngMrg31k3pRandomU01)
+#define hcrngMrg31k3pRandomInteger      _HCRNG_TAG_FPTYPE(hcrngMrg31k3pRandomInteger)
+#define hcrngMrg31k3pRandomU01Array     _HCRNG_TAG_FPTYPE(hcrngMrg31k3pRandomU01Array)
+#define hcrngMrg31k3pRandomIntegerArray _HCRNG_TAG_FPTYPE(hcrngMrg31k3pRandomIntegerArray)
+
+/*! @copybrief hcrngRandomU01()
+ *  @see hcrngRandomU01()
+ */
+HCRNGAPI _HCRNG_FPTYPE hcrngMrg31k3pRandomU01(hcrngMrg31k3pStream* stream);
+HCRNGAPI float  hcrngMrg31k3pRandomU01_float (hcrngMrg31k3pStream* stream);
+HCRNGAPI double hcrngMrg31k3pRandomU01_double(hcrngMrg31k3pStream* stream);
+
+/*! @copybrief hcrngRandomInteger()
+ *  @see hcrngRandomInteger()
+ */
+HCRNGAPI int hcrngMrg31k3pRandomInteger(hcrngMrg31k3pStream* stream, int i, int j);
+HCRNGAPI int hcrngMrg31k3pRandomInteger_float (hcrngMrg31k3pStream* stream, int i, int j);
+HCRNGAPI int hcrngMrg31k3pRandomInteger_double(hcrngMrg31k3pStream* stream, int i, int j);
+
+/*! @copybrief hcrngRandomU01Array()
+ *  @see hcrngRandomU01Array()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pRandomU01Array(hcrngMrg31k3pStream* stream, size_t count, _HCRNG_FPTYPE* buffer);
+HCRNGAPI hcrngStatus hcrngMrg31k3pRandomU01Array_float (hcrngMrg31k3pStream* stream, size_t count, float * buffer);
+HCRNGAPI hcrngStatus hcrngMrg31k3pRandomU01Array_double(hcrngMrg31k3pStream* stream, size_t count, double* buffer);
+
+/*! @copybrief hcrngRandomIntegerArray()
+ *  @see hcrngRandomIntegerArray()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pRandomIntegerArray(hcrngMrg31k3pStream* stream, int i, int j, size_t count, int* buffer);
+HCRNGAPI hcrngStatus hcrngMrg31k3pRandomIntegerArray_float (hcrngMrg31k3pStream* stream, int i, int j, size_t count, int* buffer);
+HCRNGAPI hcrngStatus hcrngMrg31k3pRandomIntegerArray_double(hcrngMrg31k3pStream* stream, int i, int j, size_t count, int* buffer);
+
+/*! @copybrief hcrngRewindStreams()
+ *  @see hcrngRewindStreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pRewindStreams(size_t count, hcrngMrg31k3pStream* streams);
+
+/*! @copybrief hcrngRewindSubstreams()
+ *  @see hcrngRewindSubstreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pRewindSubstreams(size_t count, hcrngMrg31k3pStream* streams);
+
+/*! @copybrief hcrngForwardToNextSubstreams()
+ *  @see hcrngForwardToNextSubstreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pForwardToNextSubstreams(size_t count, hcrngMrg31k3pStream* streams);
+
+/*! @copybrief hcrngMakeSubstreams()
+ *  @see hcrngMakeSubstreams()
+ */
+HCRNGAPI hcrngMrg31k3pStream* hcrngMrg31k3pMakeSubstreams(hcrngMrg31k3pStream* stream, size_t count, size_t* bufSize, hcrngStatus* err);
+
+/*! @copybrief hcrngMakeOverSubstreams()
+ *  @see hcrngMakeOverSubstreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pMakeOverSubstreams(hcrngMrg31k3pStream* stream, size_t count, hcrngMrg31k3pStream* substreams);
+
+/*! @copybrief hcrngAdvanceStreams()
+ *  @see hcrngAdvanceStreams()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pAdvanceStreams(size_t count, hcrngMrg31k3pStream* streams, int e, int c);
+
+/*! @copybrief hcrngDeviceRandomU01Array()
+ *  @see hcrngDeviceRandomU01Array()
+ */
+#ifdef HCRNG_SINGLE_PRECISION
+#define hcrngMrg31k3pDeviceRandomU01Array(...) hcrngMrg31k3pDeviceRandomU01Array_(__VA_ARGS__, HC_TRUE)
+#else
+#define hcrngMrg31k3pDeviceRandomU01Array(...) hcrngMrg31k3pDeviceRandomU01Array_(__VA_ARGS__, HC_FALSE)
+#endif
+
+/** \internal
+ *  @brief Helper function for hcrngMrg31k3pDeviceRandomU01Array()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pDeviceRandomU01Array_(size_t streamCount, hcrngMrg31k3pStream &streams,
+	size_t numberCount, Concurrency::array_view<float> &outBuffer, bool singlePrecision);
+/** \endinternal
+ */
+
+/*! @copybrief hcrngWriteStreamInfo()
+ *  @see hcrngWriteStreamInfo()
+ */
+HCRNGAPI hcrngStatus hcrngMrg31k3pWriteStreamInfo(const hcrngMrg31k3pStream* stream, FILE *file);
+
+
+#if 0
+HCRNGAPI hcrngMrg31k3pStream* hcrngMrg31k3pGetStreamByIndex(hcrngMrg31k3pStream* stream, unsigned int index);
+#endif
+
+#endif
---
+++ lib/src/include/hcRNG/private/mrg31k3p.c.h
@@ -0,0 +1,200 @@
+/*! @file mrg31k3p.c.h
+ *  @brief Code for the MRG31k3p generator common to the host and device
+ */
+
+#pragma once
+#ifndef PRIVATE_MRG31K3P_CH
+#define PRIVATE_MRG31K3P_CH
+
+#define mrg31k3p_M1 2147483647             /* 2^31 - 1 */
+#define mrg31k3p_M2 2147462579             /* 2^31 - 21069 */
+
+#define mrg31k3p_MASK12 511                /* 2^9 - 1 */
+#define mrg31k3p_MASK13 16777215           /* 2^24 - 1 */
+#define mrg31k3p_MASK2 65535               /* 2^16 - 1 */
+#define mrg31k3p_MULT2 21069
+
+#define mrg31k3p_NORM_double 4.656612873077392578125e-10  /* 1/2^31 */
+#define mrg31k3p_NORM_float  4.6566126e-10
+// hcrngMrg31k3p_A1p72 and hcrngMrg31k3p_A2p72 jump 2^72 steps forward
+static
+unsigned int hcrngMrg31k3p_A1p72[3][3] = { 
+    {1516919229,  758510237, 499121365},
+    {1884998244, 1516919229, 335398200},
+    {601897748,  1884998244, 358115744}
+};
+
+static
+unsigned int hcrngMrg31k3p_A2p72[3][3] = { 
+    {1228857673, 1496414766,  954677935},
+    {1133297478, 1407477216, 1496414766},
+    {2002613992, 1639496704, 1407477216}
+};
+
+
+hcrngStatus hcrngMrg31k3pCopyOverStreams(size_t count, hcrngMrg31k3pStream* destStreams, const hcrngMrg31k3pStream* srcStreams) restrict (amp)
+{
+    //Check params
+	if (!destStreams)
+	return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): destStreams cannot be NULL", __func__);
+	if (!srcStreams)
+	return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): srcStreams cannot be NULL", __func__);
+
+    for (size_t i = 0; i < count; i++)
+		destStreams[i] = srcStreams[i];
+
+    return HCRNG_SUCCESS;
+}
+
+/*! @brief Advance the rng one step and returns z such that 1 <= z <= mrg31k3p_M1
+ */
+static unsigned int hcrngMrg31k3pNextState(hcrngMrg31k3pStreamState* currentState) restrict (amp)
+{
+	
+	unsigned int* g1 = currentState->g1;
+	unsigned int* g2 = currentState->g2;
+	unsigned int y1, y2;
+
+	// first component
+	y1 = ((g1[1] & mrg31k3p_MASK12) << 22) + (g1[1] >> 9)
+		+ ((g1[2] & mrg31k3p_MASK13) << 7) + (g1[2] >> 24);
+
+	if (y1 >= mrg31k3p_M1)
+		y1 -= mrg31k3p_M1;
+
+	y1 += g1[2];
+	if (y1 >= mrg31k3p_M1)
+		y1 -= mrg31k3p_M1;
+
+	g1[2] = g1[1];
+	g1[1] = g1[0];
+	g1[0] = y1;
+
+	// second component
+	y1 = ((g2[0] & mrg31k3p_MASK2) << 15) + (mrg31k3p_MULT2 * (g2[0] >> 16));
+	if (y1 >= mrg31k3p_M2)
+		y1 -= mrg31k3p_M2;
+	y2 = ((g2[2] & mrg31k3p_MASK2) << 15) + (mrg31k3p_MULT2 * (g2[2] >> 16));
+	if (y2 >= mrg31k3p_M2)
+		y2 -= mrg31k3p_M2;
+	y2 += g2[2];
+	if (y2 >= mrg31k3p_M2)
+		y2 -= mrg31k3p_M2;
+	y2 += y1;
+	if (y2 >= mrg31k3p_M2)
+		y2 -= mrg31k3p_M2;
+
+	g2[2] = g2[1];
+	g2[1] = g2[0];
+	g2[0] = y2;
+
+	if (g1[0] <= g2[0])
+		return (g1[0] - g2[0] + mrg31k3p_M1);
+	else
+		return (g1[0] - g2[0]);
+}
+
+// We use an underscore on the r.h.s. to avoid potential recursion with certain
+// preprocessors.
+#define IMPLEMENT_GENERATE_FOR_TYPE(fptype) \
+	\
+	fptype hcrngMrg31k3pRandomU01_##fptype(hcrngMrg31k3pStream* stream) restrict (amp) { \
+	    return hcrngMrg31k3pNextState(&stream->current) * mrg31k3p_NORM_##fptype; \
+	} \
+	\
+	int hcrngMrg31k3pRandomInteger_##fptype(hcrngMrg31k3pStream* stream, int i, int j) restrict (amp) { \
+	    return i + (int)((j - i + 1) * hcrngMrg31k3pRandomU01_##fptype(stream)); \
+	} \
+	\
+	hcrngStatus hcrngMrg31k3pRandomU01Array_##fptype(hcrngMrg31k3pStream* stream, size_t count, fptype* buffer) restrict (amp) { \
+		if (!stream) \
+			return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): stream cannot be NULL", __func__); \
+		if (!buffer) \
+			return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): buffer cannot be NULL", __func__); \
+		for (size_t i = 0; i < count; i++)  \
+			buffer[i] = hcrngMrg31k3pRandomU01_##fptype(stream); \
+		return HCRNG_SUCCESS; \
+	} \
+	\
+	hcrngStatus hcrngMrg31k3pRandomIntegerArray_##fptype(hcrngMrg31k3pStream* stream, int i, int j, size_t count, int* buffer) restrict (amp) { \
+		if (!stream) \
+			return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): stream cannot be NULL", __func__); \
+		if (!buffer) \
+			return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): buffer cannot be NULL", __func__); \
+		for (size_t k = 0; k < count; k++) \
+			buffer[k] = hcrngMrg31k3pRandomInteger_##fptype(stream, i, j); \
+		return HCRNG_SUCCESS; \
+	}
+
+// On the host, implement everything.
+// On the device, implement only what is required to avoid hcuttering memory.
+#if defined(HCRNG_SINGLE_PRECISION) 
+IMPLEMENT_GENERATE_FOR_TYPE(float)
+#endif
+#if !defined(HCRNG_SINGLE_PRECISION) 
+IMPLEMENT_GENERATE_FOR_TYPE(double)
+#endif
+
+// Clean up macros, especially to avoid polluting device code.
+#undef IMPLEMENT_GENERATE_FOR_TYPE
+
+
+
+hcrngStatus hcrngMrg31k3pRewindStreams(size_t count, hcrngMrg31k3pStream* streams) restrict (amp)
+{
+	//Check params
+	if (!streams)
+		return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): streams cannot be NULL", __func__);
+	//Reset current state to the stream initial state
+	for (size_t j = 0; j < count; j++) {
+		streams[j].current = streams[j].substream = streams[j].initial;
+	}
+
+	return HCRNG_SUCCESS;
+}
+
+hcrngStatus hcrngMrg31k3pRewindSubstreams(size_t count, hcrngMrg31k3pStream* streams) restrict (amp)
+{
+	//Check params
+	if (!streams)
+		return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): streams cannot be NULL", __func__);
+	//Reset current state to the subStream initial state
+	for (size_t j = 0; j < count; j++) {
+		streams[j].current = streams[j].substream;
+	}
+
+	return HCRNG_SUCCESS;
+}
+
+hcrngStatus hcrngMrg31k3pForwardToNextSubstreams(size_t count, hcrngMrg31k3pStream* streams) restrict (amp)
+{
+	//Check params
+	if (!streams)
+		return hcrngSetErrorString(HCRNG_INVALID_VALUE, "%s(): streams cannot be NULL", __func__);
+	
+	for (size_t k = 0; k < count; k++) {
+		modMatVec (hcrngMrg31k3p_A1p72, streams[k].substream.g1, streams[k].substream.g1, mrg31k3p_M1);
+		modMatVec (hcrngMrg31k3p_A2p72, streams[k].substream.g2, streams[k].substream.g2, mrg31k3p_M2);
+		streams[k].current = streams[k].substream;
+	}
+
+	return HCRNG_SUCCESS;
+}
+
+hcrngStatus hcrngMrg31k3pMakeOverSubstreams(hcrngMrg31k3pStream* stream, size_t count, hcrngMrg31k3pStream* substreams) restrict (amp)
+{
+	for (size_t i = 0; i < count; i++) {
+		hcrngStatus err;
+		// snapshot current stream into substreams[i]
+		err = hcrngMrg31k3pCopyOverStreams(1, &substreams[i], stream);
+		if (err != HCRNG_SUCCESS)
+		    return err;
+		// advance to next substream
+		err = hcrngMrg31k3pForwardToNextSubstreams(1, stream);
+		if (err != HCRNG_SUCCESS)
+		    return err;
+	}
+	return HCRNG_SUCCESS;
+}
+
+#endif // PRIVATE_MRG31K3P_CH
