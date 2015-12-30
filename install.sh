235e0120ab5fd86eb643136d030fb5ab01e0a6bc
aparnasuresh <aparna@multicorewareinc.com>
2015-12-30 14:59:00 +0530

    Add test=on option in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash -e 
 # This script is invoked to install the hcRNG library and test sources
 # Preliminary version
 
 # CHECK FOR COMPILER PATH
-if [ ! -z $MCWHCCBUILD]; 
+
+if [ ! -z $MCWHCCBUILD ]; 
 then
   if [ -x "$MCWHCCBUILD/compiler/bin/clang++" ]; 
   then
@@ -32,52 +33,43 @@ if [ "x$var1" = "x" ]; then
    var1="test=off"
 fi
 
+set +e
 # MAKE BUILD DIR
 mkdir $current_work_dir/build
-mkdir $current_work_dir/build/lib
 mkdir $current_work_dir/build/test
-#mkdir $current_work_dir/build/test/bin/
+set -e
 
 # SET BUILD DIR
 build_dir=$current_work_dir/build
 
 # change to library build
-cd $build_dir/lib
+cd $build_dir
 
 # Cmake and make libhcfft: Install hcFFT
 cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir
 sudo make install
 
+
 # Build Tests
-cd $build_dir/test/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir/test/ 
+cd $build_dir/test/ && cmake -DCMAKE_C_COMPILER=$cmake_c_compiler -DCMAKE_CXX_COMPILER=$cmake_cxx_compiler -DCMAKE_CXX_FLAGS=-fPIC $current_work_dir/test/
+ 
+set +e
 mkdir $current_work_dir/build/test/src/bin/
 mkdir $current_work_dir/build/test/example/bin/
+mkdir $current_work_dir/build/test/unit/bin/
+set -e
+
 make
 
-# Simple test to confirm installation
-#$build_dir/test/src/fft 2 12
-
-# TODO: ADD More options to perform benchmark and testing
-
-
-
-
-
-#if [ "$var1" = "test=off" ]; then
-#Move to library build
-#  cd $current_work_dir/lib/build/linux
-
-
-
-#elif [ "$var1" = "test=on" ]; then
-#Move to test folder
-#  cd $current_work_dir/test/
+if [ "$var1" = "test=on" ]; then
+# Move to test folder
+  cd $current_work_dir/test/
   
 #Invoke test script
-#  ./test.sh
+  ./test.sh
 
-#fi
-#echo "HCRNG Installation Completed!"
+fi
+echo "HCRNG Installation Completed!"
 
 
 # TODO: ADD More options to perform benchmark and testing
--- test/test.sh
+++ test/test.sh
@@ -5,37 +5,20 @@
 # CURRENT_WORK_DIRECTORY
 current_work_dir=$PWD
 
-#Move to library build
-cd $current_work_dir/../lib/build/linux
-
-#Invoke build script
-sh build.sh
-
-# Install library
-sudo make install
-
-#Move to test build
-cd $current_work_dir/build/linux
-
-#Invoke build script
-sh build.sh
-
-# build test src
-make
-
-# Move to test bin
-cd $current_work_dir/build/linux/src/bin/
+# Move to src test bin
+working_dir="$current_work_dir/../build/test/src/bin/"
+cd $working_dir
 
 echo "TEST PASSED" >> testlog_temp.txt
 #Test generators
-path2mrg31k3psingle="$current_work_dir/build/linux/src/bin/mrg31k3pSingle"
-path2mrg31k3pdouble="$current_work_dir/build/linux/src/bin/mrg31k3pDouble"
-path2mrg32k3asingle="$current_work_dir/build/linux/src/bin/mrg32k3aSingle"
-path2mrg32k3adouble="$current_work_dir/build/linux/src/bin/mrg32k3aDouble"
-path2lfsr113single="$current_work_dir/build/linux/src/bin/lfsr113Single"
-path2lfsr113double="$current_work_dir/build/linux/src/bin/lfsr113Double"
-path2philox432single="$current_work_dir/build/linux/src/bin/philox432Single"
-path2philox432double="$current_work_dir/build/linux/src/bin/philox432Double"
+path2mrg31k3psingle="$working_dir/mrg31k3pSingle"
+path2mrg31k3pdouble="$working_dir/mrg31k3pDouble"
+path2mrg32k3asingle="$working_dir/mrg32k3aSingle"
+path2mrg32k3adouble="$working_dir/mrg32k3aDouble"
+path2lfsr113single="$working_dir/lfsr113Single"
+path2lfsr113double="$working_dir/lfsr113Double"
+path2philox432single="$working_dir/philox432Single"
+path2philox432double="$working_dir/philox432Double"
 
 runcmd1="$path2mrg31k3psingle >> testlog.txt"
 eval $runcmd1
@@ -65,8 +48,40 @@ echo "TEST PASSED" >> testlog.txt
 DIFF=$(diff testlog.txt testlog_temp.txt)
 if [ "$DIFF" != "" ]
 then
-    echo "TEST FAILED"
+    echo "Functionality Check ---- [ FAILED ]"
+    rm testlog_temp.txt
 else
-    echo "All Unit Tests Passed!"
+    echo "Functionality Check ---- [ PASSED ]"
+    rm testlog*
 fi
-rm testlog*
+
+# Move to gtest bin
+working_dir1="$current_work_dir/../build/test/unit/bin/"
+cd $working_dir1
+
+#Gtest functions
+path2StreamCreator="$working_dir1/test_Stream_Creator"
+path2Streams="$working_dir1/test_Streams"
+path2Substreams="$working_dir1/test_Substreams"
+path2Randomarray="$working_dir1/test_Randomarray"
+
+runcmd1="$path2StreamCreator >> gtestlog.txt"
+eval $runcmd1
+
+runcmd2="$path2Streams >> gtestlog.txt"
+eval $runcmd2
+
+runcmd3="$path2Substreams >> gtestlog.txt"
+eval $runcmd3
+
+runcmd4="$path2Randomarray >> gtestlog.txt"
+eval $runcmd4
+
+Log_file="$working_dir1/gtestlog.txt"
+if grep -q FAILED "$Log_file"; 
+then
+    echo "GTEST               ---- [ FAILED ]"   
+else
+    echo "GTEST               ---- [ PASSED ]"
+fi
+rm $working_dir1/gtestlog.txt
