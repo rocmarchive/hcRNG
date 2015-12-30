03257e0d26ccc18dd8efb2f9301c3b299903c596
aparnasuresh <aparna@multicorewareinc.com>
2015-12-30 16:45:35 +0530

    Bug fix in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -e
 # This script is invoked to uninstall the hcRNG library and test sources
 # Preliminary version
 
@@ -8,7 +9,8 @@
 current_work_dir=$PWD
 
 # Remove system wide installed lib and headers
-sudo xargs rm < $current_work_dir/build/lib/install_manifest.txt
+sudo xargs rm < $current_work_dir/build/install_manifest.txt
+sudo rm -rf /usr/local/include/hcRNG
 
 # Remove build
 sudo rm -rf $current_work_dir/build
