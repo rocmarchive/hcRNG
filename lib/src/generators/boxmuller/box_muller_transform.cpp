704787fe17ddf46492dd7af596ab303a0f39cca2
Alexandru Voicu <alex_voicu@outlook.com>
2017-05-27 15:14:26 +0100

    Correct incorrect usage of fast_math functions with double arguments. Correct incorrect use of [[hc, cpu]] restriction on Callable passed to p_f_e - this can ONLY be [[hc]].

--- lib/src/generators/boxmuller/box_muller_transform.cpp
+++ lib/src/generators/boxmuller/box_muller_transform.cpp
@@ -7,13 +7,15 @@ hcrngStatus box_muller_transform_single(hc::accelerator_view &accl_view, float m
     long size = (numberCount/2 + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
     hc::extent<1> grdExt(size);
     hc::tiled_extent<1> t_ext(grdExt, BLOCK_SIZE);
-    hc::parallel_for_each(accl_view, t_ext, [ = ] (hc::tiled_index<1> tidx) [[hc, cpu]] {
+    hc::parallel_for_each(accl_view, t_ext, [ = ] (hc::tiled_index<1> tidx) [[hc]] {
       float z0, z1;
       int gid = tidx.global[0];
       if (gid < numberCount/2)
       {
-        z0 = fast_math::sqrt(-2.0 * fast_math::log(Randomnum[gid * 2])) * fast_math::cos(two_pi * Randomnum[gid * 2 + 1]);
-        z1 = fast_math::sqrt(-2.0 * fast_math::log(Randomnum[gid * 2])) * fast_math::sin(two_pi * Randomnum[gid * 2 + 1]);
+        z0 = fast_math::sqrt(-2.0f * fast_math::log(Randomnum[gid * 2])) *
+            fast_math::cos(two_pi * Randomnum[gid * 2 + 1]);
+        z1 = fast_math::sqrt(-2.0f * fast_math::log(Randomnum[gid * 2])) *
+            fast_math::sin(two_pi * Randomnum[gid * 2 + 1]);
         Randomnum[gid * 2] = z0 * sigma + mu;
         Randomnum[gid * 2 + 1] = z1 * sigma + mu;
       }
@@ -44,13 +46,15 @@ hcrngStatus box_muller_transform_double(hc::accelerator_view &accl_view, double 
     long size = (numberCount/2 + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
     hc::extent<1> grdExt(size);
     hc::tiled_extent<1> t_ext(grdExt, BLOCK_SIZE);
-    hc::parallel_for_each(accl_view, t_ext, [ = ] (hc::tiled_index<1> tidx) [[hc, cpu]] {
+    hc::parallel_for_each(accl_view, t_ext, [=] (hc::tiled_index<1> tidx) [[hc]] {
       double z0, z1;
       int gid = tidx.global[0];
       if (gid < numberCount/2)
       {
-        z0 = fast_math::sqrt(-2.0 * fast_math::log(Randomnum[gid * 2])) * fast_math::cos(two_pi * Randomnum[gid * 2 + 1]);
-        z1 = fast_math::sqrt(-2.0 * fast_math::log(Randomnum[gid * 2])) * fast_math::sin(two_pi * Randomnum[gid * 2 + 1]);
+        z0 = precise_math::sqrt(-2.0 * precise_math::log(Randomnum[gid * 2])) *
+            precise_math::cos(two_pi * Randomnum[gid * 2 + 1]);
+        z1 = precise_math::sqrt(-2.0 * precise_math::log(Randomnum[gid * 2])) *
+            precise_math::sin(two_pi * Randomnum[gid * 2 + 1]);
         Randomnum[gid * 2] = z0 * sigma + mu;
         Randomnum[gid * 2 + 1] = z1 * sigma + mu;
       }
