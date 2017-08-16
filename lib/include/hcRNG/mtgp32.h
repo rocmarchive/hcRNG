e97eee7737b81befa6ee4d875c8247aaf1ba3911
Neelakandan <neelakandan@multicorewareinc.com>
2017-08-16 10:22:59 +0530

    Redesign mtgp interface to accomodate compilation with any C++ compiler

--- lib/include/hcRNG/mtgp32.h
+++ lib/include/hcRNG/mtgp32.h
@@ -3,384 +3,52 @@
 
 #pragma once
 
-// FIXME: Better to inline every amp-restricted functions and make it static
 #include "hcRNG/private/mtgp32-fast.h"
-#include <hc.hpp>
-#include <hc_defines.h>
+#include "hcRNG.h"
+// Opaque structure declarations
 
-#define MTGP32_MEXP 11213
-#define MTGP32_N 351
-#define MTGP32_FLOOR_2P 256
-#define MTGP32_CEIL_2P 512
-#define MTGP32_TN MTGP32_FLOOR_2P // TBL_NUMBER, do not exceed 256, i.e. WAVEFRONT_SIZE
-#define MTGP32_LS (MTGP32_TN * 3)
-#define MTGP32_TS 16              // TBL_SIZE
-#define HcRAND_GROUP_NUM 200     // < MTGP32_TN, user specifies USER_GROUP_NUM
-#define MTGP32_STATE_SIZE 1024    // The effective state number is =351
-#define MTGP32_STATE_MASK 1023
-#define USER_GROUP_NUM 64         // < HcRAND_GROUP_NUM
+typedef struct mtgp32_kernel_params mtgp32_kernel_params_t;
 
-#ifndef DIVUP
-#define DIVUP(x, y) (((x) + (y) - 1) / (y))
-#endif
-#define BLOCK_SIZE 256
-#define MAX_NUM_BLOCKS 64
+typedef struct hcrngStateMtgp32 hcrngStateMtgp32_t;
 
+typedef hcrngStatus_ hcrngStatus_t;
 
-// mtgp32_kernel_params
-typedef struct mtgp32_kernel_params {
-  uint32_t offset[USER_GROUP_NUM]; // size: USER_GROUP_NUM
-  uint32_t index[USER_GROUP_NUM];  // size: USER_GROUP_NUM
-  uint32_t d_status[USER_GROUP_NUM * MTGP32_STATE_SIZE]; // extent<2>(USER_GROUP_NUM, MTGP32_STATE_SIZE)
-  // mtgp32 kernel params
-  uint32_t mexp_tbl[HcRAND_GROUP_NUM];                      // size: 1. Redundant
-  uint32_t param_tbl[HcRAND_GROUP_NUM * MTGP32_TN];         // extent<2>(HcRAND_GROUP_NUM, MTGP32_TN)
-  uint32_t temper_tbl[HcRAND_GROUP_NUM * MTGP32_TN];        // extent<2>(HcRAND_GROUP_NUM, MTGP32_TN)
-  uint32_t single_temper_tbl[HcRAND_GROUP_NUM * MTGP32_TN]; // extent<2>(HcRAND_GROUP_NUM, MTGP32_TN)
-  uint32_t pos_tbl[MTGP32_TN];  // size: MTGP32_TN
-  uint32_t sh1_tbl[MTGP32_TN];  // size: MTGP32_TN
-  uint32_t sh2_tbl[MTGP32_TN];  // size: MTGP32_TN
-  uint32_t mask[1];             // size: 1
-} mtgp32_kernel_params;
+namespace hc {
+class accelerator;
+class accelerator_view;
+};
+
+#define __device__ __attribute__((hc))
+#define __host__ __attribute__((cpu))
+
+//  Set up initial states for the mtgp32 generator. 
+__host__ hcrngStatus_t hcrngMakeMTGP32KernelState (hc::accelerator_view accl_view, hcrngStateMtgp32_t* s, mtgp32_params_fast_t params[], mtgp32_kernel_params_t* k, int  n, unsigned long long seed );
+
+//     Set up constant parameters for the mtgp32 generator. 
+__host__ hcrngStatus_t hcrngMakeMTGP32Constants ( hc::accelerator_view accl_view, const mtgp32_params_fast_t params[], mtgp32_kernel_params_t* p );
 
 
-// Structure of hcrngStateMtgp32
-typedef struct hcrngStateMtgp32 {
-  uint32_t s[MTGP32_STATE_SIZE];
-  uint32_t offset;
-  uint32_t pIdx;
-  mtgp32_kernel_params* k;  // points to all kernel parameters
-} hcrngStateMtgp32;
+//  Return a log-normally distributed float from an MTGP32 generator. 
+__device__ float hcrng_log_normal ( hcrngStateMtgp32_t* state, float  mean, float  stddev);
 
-void hcrngStateMtgp32_init(
-    hc::accelerator_view accl_view, hcrngStateMtgp32* s);
-void hcrngStateMtgp32_release(hcrngStateMtgp32* s);
-void hcrngStateMtgp32_copy_D2H(void* src, void* dst);
-void hcrngStateMtgp32_copy_H2D(void* src, void* dst);
-
-// Device APIs
-int mtgp32_init_params_kernel(
-    hc::accelerator_view accl_view,
-    const mtgp32_params_fast_t* params,
-    mtgp32_kernel_params*& s);
-int mtgp32_init_seed_kernel(
-    hc::accelerator_view accl_view,
-    hcrngStateMtgp32* state,
-    mtgp32_kernel_params* p,
-    int n,
-    unsigned long seed);
-/**
- * The function of the recursion formula calculation.
- *
- * @param[in] X1 the farthest part of state array.
- * @param[in] X2 the second farthest part of state array.
- * @param[in] Y a part of state array.
- * @return output
- */
-static
-inline
-uint32_t para_rec(
-    uint32_t* param_tbl,
-    uint32_t sh1,
-    uint32_t sh2,
-    uint32_t X1,
-    uint32_t X2,
-    uint32_t Y,
-    uint32_t mask) [[cpu]][[hc]]
-{
-  uint32_t X = (X1 & mask) ^ X2;
-  uint32_t MAT;
-
-  X ^= X << sh1;
-  Y = X ^ (Y >> sh2);
-  MAT = param_tbl[Y & 0x0f];
-  return Y ^ MAT;
-}
-
-/**
- * The tempering function.
- *
- * @param[in] mtgp mtgp32 structure
- * @param[in] V the output value should be tempered.
- * @param[in] T the tempering helper value.
- * @return the tempered value.
- */
-static
-inline
-uint32_t temper(uint32_t* temper_tbl, uint32_t V, uint32_t T) [[cpu]][[hc]]
-{
-  uint32_t MAT;
-
-  T ^= T >> 16;
-  T ^= T >> 8;
-  MAT = temper_tbl[T & 0x0f];
-  return V ^ MAT;
-}
-
-#define HcRAND_2POW32_INV (2.3283064e-10f)
-#define HcRAND_SQRT2 (-1.4142135f)
-#define HcRAND_SQRT2_DOUBLE (-1.4142135623730951)
-
-// in one workgroup
-inline static
-unsigned int hcrng(
-    hcrngStateMtgp32 *state) [[hc]]
-{  
-
-  const uint32_t* av_param_tbl = (state[0].k->param_tbl); 
-  const uint32_t* av_temper_tbl = (state[0].k->temper_tbl); 
-  const uint32_t* av_sh1_tbl = (state[0].k->sh1_tbl); 
-  const uint32_t* av_sh2_tbl = (state[0].k->sh2_tbl); 
-  uint32_t* av_offset = (state[0].k->offset); 
-  const uint32_t* av_index = (state[0].k->index); 
-  const uint32_t* av_pos_tbl = (state[0].k->pos_tbl); 
-  const uint32_t* av_mask = (state[0].k->mask); 
-  const uint32_t* av_d_status = (state[0].k->d_status); 
-   
-  unsigned int t = (hc_get_group_size(2) * hc_get_group_size(1) * hc_get_workitem_id(2)) + (hc_get_group_size(0) * hc_get_workitem_id(1)) + hc_get_workitem_id(0);
-  unsigned int d = hc_get_group_size(2) * hc_get_group_size(1) * hc_get_group_size(0) ;
-  uint32_t* pParam_tbl = const_cast<uint32_t*>(&av_param_tbl[0]);
-  uint32_t* pTemper_tbl = const_cast<uint32_t*>(&av_temper_tbl[0]);
-  uint32_t* d_status = const_cast<uint32_t*>(&av_d_status[0]);
-  uint32_t offset = av_offset[0];
-  uint32_t sh1 = av_sh1_tbl[0];
-  uint32_t sh2 = av_sh2_tbl[0];
-  int index = av_index[0];
-  int pos = av_pos_tbl[index];
-  unsigned int r;
-  unsigned int o;
-  r = para_rec(pParam_tbl, sh1, sh2, d_status[(t + offset) & MTGP32_STATE_MASK],
-           d_status[(t + offset + 1) & MTGP32_STATE_MASK],
-           d_status[(t + offset + pos) & MTGP32_STATE_MASK],
-           av_mask[0]);
-  d_status[(t + offset + MTGP32_N) & MTGP32_STATE_MASK] = r;
-  o = temper(pTemper_tbl, r, d_status[(t + offset + pos -1) & MTGP32_STATE_MASK]);
-
-  // Thread Synchronization
-  hc_barrier(CLK_LOCAL_MEM_FENCE);
-
-  if (t == 0) {
-    av_offset[0] = (av_offset[0] + d) & MTGP32_STATE_MASK;
-  }
-
-  // Thread Synchronization
-  hc_barrier(CLK_LOCAL_MEM_FENCE);
-  return o;
-}
-
-inline static
-float _hcrng_uniform(unsigned int x) [[cpu]][[hc]]
-{
-  return x * HcRAND_2POW32_INV + (HcRAND_2POW32_INV/2.0f);
-}
+//  Return a log-normally distributed double from an MTGP32 generator. 
+__device__ double hcrng_log_normal_double ( hcrngStateMtgp32_t* state, double  mean, double  stddev );
 
 
-inline float hcrng_uniform(
-    hcrngStateMtgp32 *state) [[hc]]
-{
-  unsigned int x = hcrng(
-      state);
-  return _hcrng_uniform(x);
-}
+//   Return a normally distributed float from a MTGP32 generator. 
+__device__ float hcrng_normal ( hcrngStateMtgp32_t* state );
 
-inline static
-double do_erfcinv(float x, bool refine) [[cpu]][[hc]]
-{
-  // Coefficients of rational approximation.
-  const double a[] =
-    { -2.806989788730439e+01,  1.562324844726888e+02,
-      -1.951109208597547e+02,  9.783370457507161e+01,
-      -2.168328665628878e+01,  1.772453852905383e+00 };
-  const double b[] =
-    { -5.447609879822406e+01,  1.615858368580409e+02,
-      -1.556989798598866e+02,  6.680131188771972e+01,
-      -1.328068155288572e+01 };
-  const double c[] =
-    { -5.504751339936943e-03, -2.279687217114118e-01,
-      -1.697592457770869e+00, -1.802933168781950e+00,
-       3.093354679843505e+00,  2.077595676404383e+00 };
-  const double d[] =
-    {  7.784695709041462e-03,  3.224671290700398e-01,
-       2.445134137142996e+00,  3.754408661907416e+00 };
-
-  static const double spi2 =  8.862269254527579e-01; // sqrt(pi)/2.
-  //static const double pi = 3.14159265358979323846;
-  static const double pbreak = 0.95150;
-  double y;
-
-  // Select case.
-  if (x <= 1+pbreak && x >= 1-pbreak) {
-    // Middle region.
-    const double q = 0.5*(1-x), r = q*q;
-    const double yn =
-        (((((a[0]*r + a[1])*r + a[2])*r + a[3])*r + a[4])*r + a[5])*q;
-    const double yd =
-        ((((b[0]*r + b[1])*r + b[2])*r + b[3])*r + b[4])*r + 1.0;
-    y = yn / yd;
-  } else if (x < 2.0 && x > 0.0) {
-    // Tail region.
-    const double q =
-        x < 1 ? hc::precise_math::sqrt(-2*hc::precise_math::log((double)0.5*x))
-              : hc::precise_math::sqrt(-2*hc::precise_math::log((double)0.5*(2-x)));
-    const double yn = ((((c[0]*q + c[1])*q + c[2])*q + c[3])*q + c[4])*q + c[5];
-    const double yd = (((d[0]*q + d[1])*q + d[2])*q + d[3])*q + 1.0;
-    y = yn / yd;
-    if (x < 1-pbreak)
-      y *= -1;
-  } else if (x == 0.0) {
-    return 0; //octave_Inf;
-  } else if (x == 2.0) {
-    return 0; //-octave_Inf;
-  } else {
-    return 1; //octave_NaN;
-  }
-
-  if (refine) {
-    // One iteration of Halley's method gives full precision.
-    double u = (erf(y) - (1-x)) * spi2 * exp (y*y);
-    y -= u / (1 + y*u);
-  }
-
- return y;
-}
+//  Return a normally distributed double from an MTGP32 generator. 
+__device__ double hcrng_normal_double ( hcrngStateMtgp32_t* state);
 
 
-inline static
-float my_erfcinvf(float x) [[cpu]][[hc]]
-{
-  return do_erfcinv (x, false);
-}
+//   Return a uniformly distributed float from a MTGP32 generator. 
+__device__ float hcrng_uniform ( hcrngStateMtgp32_t* state );
 
-inline static
-
-float _hcrng_normal_icdf(unsigned int x) [[cpu]][[hc]]
-{
-  float s = HcRAND_SQRT2;
-  // Mirror to avoid loss of precision
-  if (x > 0x80000000UL) {
-    x = 0xffffffffUL - x;
-    s = -s;
-  }
-  float p = x * HcRAND_2POW32_INV + (HcRAND_2POW32_INV/2.0f);
-  // p is in (0, 0.5], 2p is in (0, 1]
-  return s * my_erfcinvf(2.0f * p);
-}
-
-inline static
-float hcrng_normal(
-    hcrngStateMtgp32 *state) [[hc]]
-{
-  unsigned int x = hcrng(
-      state);
-  return _hcrng_normal_icdf(x);
-}
-
-inline static
-double hcrng_log_normal(
-    hcrngStateMtgp32 *state,
-    double mean,
-    double stddev) [[hc]]
-{
-  unsigned int x = hcrng(
-      state);
-  return hc::precise_math::exp(mean + ((double)stddev * _hcrng_normal_icdf(x)));
-}
-
-// User defined wrappers
-template<typename T>
-inline static
-void user_log_normal_kernel(
-    hc::accelerator_view accl_view,
-    hcrngStateMtgp32 *s,
-    T* &av_result,
-    double mean,
-    double stddev,
-    uint32_t size)
-{
-  int rounded_size = DIVUP(size, BLOCK_SIZE) * BLOCK_SIZE;
-  int blocks = std::min((int)DIVUP(size, BLOCK_SIZE), MAX_NUM_BLOCKS);
-  hc::extent<1> ext(blocks*BLOCK_SIZE);
-  hc::tiled_extent<1> t_ext = ext.tile(BLOCK_SIZE);
-
-  hc::parallel_for_each(
-      accl_view, t_ext, [=] (hc::tiled_index<1>& tidx) [[hc]] {
-    uint32_t threadId = tidx.global[0];
-    uint32_t groupId = tidx.tile[0];
-    if (groupId >= USER_GROUP_NUM)
-      return;
-   for (int i = threadId; i < rounded_size; i += BLOCK_SIZE * MAX_NUM_BLOCKS) {
-      double x = hcrng_log_normal(
-          s,
-          mean,
-          stddev);
-      if (i < size) {
-        av_result[i] = x;
-      }
-    }
-  }).wait();
-}
-
-// User defined wrappers
-template <typename UnaryFunction, typename T>
-inline void user_uniform_kernel(
-    hc::accelerator_view accl_view,
-    hcrngStateMtgp32 *s,
-    T* &av_result,
-    uint32_t size,
-    UnaryFunction f)
-{
-  int rounded_size = DIVUP(size, BLOCK_SIZE) * BLOCK_SIZE;
-  int blocks = std::min((int)DIVUP(size, BLOCK_SIZE), MAX_NUM_BLOCKS);
-  hc::extent<1> ext(blocks*BLOCK_SIZE);
-  hc::tiled_extent<1> t_ext = ext.tile(BLOCK_SIZE);
-  hc::parallel_for_each(
-      accl_view, t_ext, [=] (hc::tiled_index<1>& tidx) [[hc]] {
-    int threadId = tidx.global[0];
-    int groupId = tidx.tile[0];
-    if (groupId >= USER_GROUP_NUM)
-      return;
-
-    for (int i = threadId; i < rounded_size; i += BLOCK_SIZE * MAX_NUM_BLOCKS) {
-      float x = hcrng_uniform(
-          s);
-      if (i < size) {
-        double y = f(x);
-        av_result[i] = y;
-      }
-    }
-  }).wait();
-}
-
-
-template <typename UnaryFunction, typename T>
-inline void user_normal_kernel(
-    hc::accelerator_view accl_view,
-    hcrngStateMtgp32 *s,
-    T* &av_result,
-    uint32_t size,
-    UnaryFunction f)
-{
-  int rounded_size = DIVUP(size, BLOCK_SIZE) * BLOCK_SIZE;
-  int blocks = std::min((int)DIVUP(size, BLOCK_SIZE), MAX_NUM_BLOCKS);
-  hc::extent<1> ext(blocks*BLOCK_SIZE);
-  hc::tiled_extent<1> t_ext = ext.tile(BLOCK_SIZE);
-  hc::parallel_for_each(
-      accl_view, t_ext, [=] (hc::tiled_index<1>& tidx) [[hc]] {
-    uint32_t threadId = tidx.global[0];
-    uint32_t groupId = tidx.tile[0];
-    if (groupId >= USER_GROUP_NUM)
-      return;
-
-    for (int i = threadId; i < rounded_size; i += BLOCK_SIZE * MAX_NUM_BLOCKS) {
-      float x = hcrng_normal(s);
-      if (i < size) {
-        double y = f(x);
-        av_result[i] = y;
-      }
-    }
-  }).wait();
-}
-
+//  Return a uniformly distributed double from an MTGP32 generator. 
+__device__ double hcrng_uniform_double ( hcrngStateMtgp32_t* state);
 
 
 #endif // MTGP32_H_
+
+
