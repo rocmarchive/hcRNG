7d066ca79d6665f950dd5cb0e534bcd894456ee0
Neelakandan <neelakandan@multicorewareinc.com>
2017-09-04 06:06:45 +0000

    Add install and uninstall scripts
    Also remove _hcc and _nvcc extensions
    
    Add hardcoded symlink paths under postinst debian install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+# bash script to install the library 
+
+#invoke build script with install argument 
+
+#also do a preliminary basic testing
+
+sh build.sh --test=basic 
+
+# Use dpkg package installer to install the generated debian package
+
+sudo dpkg -i $PWD/build/hcrng*.deb
+
+sudo ldconfig
